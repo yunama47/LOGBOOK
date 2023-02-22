3952c1b58d840a07f02ca38d75e8190b4aae7264
yunama47 <yunama1236@gmail.com>
2023-02-22 14:40:59 +0800

    add spelling check and make week2 logs

--- logbook.py
+++ logbook.py
@@ -1,5 +1,8 @@
 import re
 import string
+from spellchecker import SpellChecker
+
+spell = SpellChecker()
 
 def words_counter(text,type_='weekly'):
     '''
@@ -9,9 +12,13 @@ def words_counter(text,type_='weekly'):
     maxw = 150 if (type_ == 'weekly') else 50
     minw = 100 if (type_ == 'weekly') else 20
 
-    # text = text.translate(str.maketrans("","",string.punctuation 
-    #                                         + string.digits))
+    text = text.translate(str.maketrans("","",string.punctuation 
+                                            + string.digits))
     words = re.findall(r'\w+',text)
-    print(f'number of words is {len(words)} ,should be around {minw} - {maxw} words')
+    misspelled = spell.unknown(words) or "None"
+    print(f'number of words is {len(words)} ,should be between {minw} - {maxw} words')
+    print('words that may be misspelled : ',misspelled)
     return words
 
+
+    
