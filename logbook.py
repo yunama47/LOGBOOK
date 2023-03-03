8ceafd49dac83740c708beeb2e98d1cbdef8a117
yunama47 <yunama1236@gmail.com>
2023-03-03 22:05:54 +0800

    week 3 done, and some revision on logbook module

--- logbook.py
+++ logbook.py
@@ -1,24 +1,45 @@
 import re
-import string
 from spellchecker import SpellChecker
+import pyperclip
 
+from tkinter import *
+from tkinter import messagebox
+
+class List (list):
+    def __sub__(self,other_list):
+        for x in other_list:
+            if x in self:
+                self.remove(x)
+        return self
+class String(str):
+    def flatten(self):
+        return " ".join([line.strip() for line in self.splitlines()])
+        
+knownword = List(['ilt','bangkit','lusi','am','pm','ml','wita','coursera','dicoding','ss'])
 spell = SpellChecker()
 
-def words_counter(text,type_='weekly'):
+def write(text,type_='weekly'):
     '''
     This function used to count the number of words in the text given on the parameter.
     '''
     assert isinstance(text,str) ,"parameter must be string"
     maxw = 150 if (type_ == 'weekly') else 50
     minw = 100 if (type_ == 'weekly') else 20
+    text = String(text)
+    words = re.findall(r'[^0-9!"#$%&\'()*+,-./:;<=>?@^_`{|}~\\\[\] ]+',text.flatten())
+    misspelled = List(spell.unknown(words)) - knownword or "None"
+    res = f'number of words is {len(words)} ,should be between {minw} - {maxw} words\nwords that may be misspelled : {misspelled}'
+    pyperclip.copy(text.flatten())
+    print(res)
+    return res
 
-    text = text.translate(str.maketrans("","",string.punctuation 
-                                            + string.digits))
-    words = re.findall(r'\w+',text)
-    misspelled = spell.unknown(words) or "None"
-    print(f'number of words is {len(words)} ,should be between {minw} - {maxw} words')
-    print('words that may be misspelled : ',misspelled)
-    return words
+if __name__ == '__main__':
+    Text_ = String('''Today i have 2 activity from bangkit. One is mandatory and the other one is not mandatory
+                        10:00-11:00(WITA) - [Mandatory] I did the english pre-test from bangkit.
+                        16:30-18:00(WITA) - [Non-Mandatory] I joined guest speaker session #1 at bangkit youtube channel''')
+
+    print(spell.split_words(Text_))
+    print(re.findall(r'[^0-9!"#$%&\'()*+,-./:;<=>?@^_`{|}~\\\[\] ]+',Text_.flatten()))
+    pass
 
 
-    
